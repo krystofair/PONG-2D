bb2bd2531b8dd5c2254116f723eefb93c446efdd
qpyton <33362868+Christofair@users.noreply.github.com>
2019-04-13 17:08:12 +0200

    Pierwsza podstawowa konfiguracja(cmake)

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+using namespace std;
+
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+    //sf::Window window(sf::VideoMode(800, 600), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1024,800), "cocoo", sf::Style::Fullscreen);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.display();
+    }
+
+    return 0;
+}
