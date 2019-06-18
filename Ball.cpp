adaf4788e6ebdb81b9b59c112a444bdd6873b88d
Mikołaj <43964110+fiko222@users.noreply.github.com>
2019-06-18 19:11:32 +0200

    Update Ball.cpp
    Wygwizdano połowę kodu essa

--- Ball.cpp
+++ Ball.cpp
@@ -6,41 +6,9 @@ using namespace std;
 
 Ball::Ball()
 {
-	//maxAxisSpeed = 12;
 	circleShape.setRadius(10);
 	circleShape.setFillColor(sf::Color::White);
-	//GenerateVelocity();
 }
-
-/*
-float RandomNumber(float Min, float Max)
-{
-	return ((float(rand()) / float(RAND_MAX)) * (Max - Min));
-}
-
-void Ball::GenerateVelocity()
-{
-	float x, y;
-
-	//utworzenie poziomej sk³dowej prêdkosci
-	x = RandomNumber(1.5, 7) - RandomNumber(1.5, 7);
-
-	//Jeœli pozioma sk³adowa prêdkosci za niska utowrzyæ inn¹
-	if (abs(x) < 1)
-	{
-		GenerateVelocity();
-		return;
-	}
-	//Utworzenie pionowej sk³¹dowej prêdkosci
-	y = RandomNumber(1.5, 7) - RandomNumber(1.5, 7);
-	velocity = sf::Vector2f(x, y);
-	float magnitude = sqrt(pow(velocity.x, 2) + pow(velocity.y, 2));
-	velocity.x = velocity.x / magnitude;
-	velocity.y = velocity.y / magnitude;
-	velocity.x = velocity.x * maxAxisSpeed;
-	velocity.y = velocity.y * maxAxisSpeed;
-}
-*/
 
 Ball::~Ball()
 {
@@ -75,16 +43,6 @@ sf::Vector2f Ball::GetVelocity()
 sf::Vector2f Ball::GetPosition()
 {
 	return circleShape.getPosition();
-}
-
-void Ball::Rest()
-{
-	//GenerateVelocity();
-}
-
-void Ball::Bounce(int a, int b)
-{
-	velocity = sf::Vector2f(velocity.x * a, velocity.y * b);
 }
 
 float Ball::GetSpeed()
